7513715dc80bd3303d6474f87696967c4bc508b0
Rishabh Kushwah <kushwahrishabh159@gmail.com>
2024-12-14 01:41:56 +0530

    routes and multer

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middleware/multer.middleware.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+
+const router = Router();
+
+router
+  .route("/register-user")
+  .post(upload.fields([{ name: "avatar", maxCount: 1 }]), registerUser);
+
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+
+export default router;
